c97d7f35347907cd82a085fe849862ef8821bc3b
Terry Cheng <chengyh@shinetechchina.com>
2019-12-14 13:45:51 +0800

    Add the websocket demo.

---
+++ graphqlServer/src/main/java/org/penguin/project/tutorial/model/ServerMessage.java
@@ -0,0 +1,13 @@
+package org.penguin.project.tutorial.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServerMessage {
+
+    private String content;
+}
