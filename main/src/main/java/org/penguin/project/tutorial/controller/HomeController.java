ca6b70279d50023d8abe01e082d55125d64ba5a8
Terry Cheng <chengyh@shinetechchina.com>
2019-12-23 17:30:20 +0800

    Add chinese chars in property files to html template.

--- main/src/main/java/org/penguin/project/tutorial/controller/HomeController.java
+++ main/src/main/java/org/penguin/project/tutorial/controller/HomeController.java
@@ -1,16 +1,43 @@
 package org.penguin.project.tutorial.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 
 @Slf4j
-@RestController
+@Controller
 public class HomeController {
 
+    @Value("${tutorial.app.title}")
+    String title;
+
     @GetMapping(path = "/")
+    @ResponseBody
     public String all() {
-        return "Spring boot Tutorial Classic——Home";
+        return "Spring boot Tutorial Classic —— Home";
+    }
+
+    @GetMapping("/album/{id}")
+    public String pcAlbum(@PathVariable int id, Model model, HttpServletRequest request) {
+        String userAgent = request.getHeader("user-agent");
+        Enumeration<String> names = request.getHeaderNames();
+        while (names.hasMoreElements()) {
+            String name = names.nextElement();
+            log.info("{} = {}", name, request.getHeader(name));
+        }
+        log.info("/pc/album?id=" + id);
+        model.addAttribute("id", id);
+        model.addAttribute("userAgent", userAgent);
+        model.addAttribute("title", title);
+        log.info("属性: {}", title);
+        return "/pc/album";
     }
 
 }
